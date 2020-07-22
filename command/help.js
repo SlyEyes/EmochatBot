e15906dac387e24d380fe2cd97434989afbcfc5c
SlyEyes <raphoro.d@gmail.com>
2020-07-22 23:31:01 +0200

    new command & command move to index

--- command/help.js
+++
@@ -1,6 +0,0 @@
-// Help command
-bot.on('message', message => {
-  if (message.content === config.prefix + 'help') {
-    message.reply("help test")
-  }
-})
--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 
 // Bot's base
 const packages = require("./packages.json")
-const auth = require("./src/auth.json")
-const config = require("./config.json")
+const auth = require("./auth.json")
 const { Client } = require('discord.js')
 const Discord = require('discord.js')
 const bot = new Client()
+const config = require("./config.json")
 
 bot.on('ready', function () {
   console.log("EmochatBot connected !\nTo turn off the bot, just closed this window, otherwise leave it open\n \nCreate by SłγЗγєѕ#5557\nDevelopper's Github : https://github.com/SlyEyes")
@@ -18,6 +18,10 @@ bot.login(auth.token)
 // Help command
 bot.on('message', message => {
     if (message.content === config.prefix + 'help') {
-      message.reply("help test")
+        message.reply("Use " + config.prefix + "help [name of the command]")
     }
-  })
+})
+
+// Emochat command
+
+// List command
