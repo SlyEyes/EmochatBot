11606008095dc5e3aa1e8af6284dec1322dfc2b9
SlyEyes <raphoro.d@gmail.com>
2020-07-23 16:09:35 +0200

    help and list command fusion & base of Emoticat

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 
 // Bot's base
 const packages = require("./packages.json")
-const auth = require("./auth.json")
-const { Client } = require('discord.js')
 const Discord = require('discord.js')
+const { Client } = require('discord.js')
+const auth = require("./auth.json")
 const bot = new Client()
 const config = require("./config.json")
 
@@ -15,25 +15,39 @@ bot.on('ready', function () {
 
 bot.login(auth.token)
 
-// Help command
-bot.on('message', message => {
-    if (message.content === config.prefix + 'help') {
-        message.reply("\nUse " + config.prefix + "help [name of the command] for help on a command\nUse " + config.prefix + "list for a list of the command and the Emoticat")
-    }
-})
+// Help List command (<help)
 
 bot.on('message', message => {
-  if (message.content === config.prefix + 'help list') {
-      message.reply("\nThis command display a list of all the command and Emoticat available")
-  }
-})
-
-// List command
-
-bot.on('message', message => {
-  if (message.content === config.prefix + 'list') {
-      message.reply("\nThe next commands are available :\nhelp [name of the command]\nlist")
+  if (message.content === config.prefix + 'help') {
+      message.reply("\n__Hi there, the next commands are available for EmoticatBot :__\n**<help** [*Display a list of all the available commands and their effects*]\n**<aaah_cat** [*Aaah cat Emoticat*]\n**<relax_cat** [*Relax cat Emoticat*]\n**<sad_cat** [*Sad cat Emoticat*]\n**<hi_cat** [*Hi cat Emoticat*]\n**<raaage_cat** [*Raaage cat Emoticat*]\n**<lil_cat** [*Lil cat Emoticat*]\n**<dance_cat** [*Dance cat Emoticat*]\n**<demon_dance_cat** [*Demon dance cat Emoticat*]\n(*Go to https://github.com/users/SlyEyes/projects/2 to follow the developpement of this bot !*)")
   }
 })
 
 // Emoticat command
+
+  // <aaah_cat
+bot.on('message', message => {
+  if (message.content === config.prefix + 'aaah_cat') {
+      message.reply({files: ["https://i.imgur.com/idtdsZK.png"]})
+  }
+})
+
+  // <relax_cat
+
+
+  // <sad_cat
+
+
+  // <hi_cat
+
+
+  // <raaage_cat
+
+
+  // <lil_cat
+
+
+  // <dance_cat
+
+
+  // <demon_dance_cat
