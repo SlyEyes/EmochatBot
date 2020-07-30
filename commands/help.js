14d90054f6b226b11e502de358b4b9c628d4b7e2
SlyEyes <raphoro.d@gmail.com>
2020-07-30 15:37:02 +0200

    help list now sended in dm

--- commands/help.js
+++ commands/help.js
@@ -2,10 +2,12 @@ module.exports = {
     name: 'help',
     description: 'Display a list of the available commands',
     execute(message, args) {
-        message.reply({embed : {
-            color: 0x00ffff,
-            title: '\n__Hi there, the next commands are available for EmoticatBot :__\n\n',
-            description: '**<help** [*Display a list of all the available commands and their effects*]\n**<aaah** [*Aaah cat Emoticat*]\n**<relax** [*Relax cat Emoticat*]\n**<sad** [*Sad cat Emoticat*]\n**<hi** [*Hi cat Emoticat*]\n**<raaage** [*Raaage cat Emoticat*]\n**<lil** [*Lil cat Emoticat*]\n**<dance** [*Dance cat Emoticat*]\n**<dance_demon** [*Demon dance cat Emoticat*]\n\n(*Go to https://github.com/users/SlyEyes/projects/2 to follow the developpement of this bot !*)',
-          }})
+        message.author.createDM().then(channel => {
+            channel.send({embed : {
+                color: 0x00ffff,
+                title: `\n__Hi ${message.author.username}, the next commands are available for EmoticatBot :__\n\n`,
+                description: '**<help** [*Display a list of all the available commands and their effects*]\n**<aaah** [*Aaah cat Emoticat*]\n**<relax** [*Relax cat Emoticat*]\n**<sad** [*Sad cat Emoticat*]\n**<hi** [*Hi cat Emoticat*]\n**<raaage** [*Raaage cat Emoticat*]\n**<lil** [*Lil cat Emoticat*]\n**<dance** [*Dance cat Emoticat*]\n**<dance_demon** [*Demon dance cat Emoticat*]\n\n(*Go to https://github.com/users/SlyEyes/projects/2 to follow the developpement of this bot !*)',
+            }})
+        })
     }
 }
